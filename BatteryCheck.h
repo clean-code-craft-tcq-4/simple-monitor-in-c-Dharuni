c7644a8e3fa525c16de29fbccdc98dd324336d66
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-07-05 21:25:25 +0530

    Update BatteryCheck.h

--- BatteryCheck.h
+++ BatteryCheck.h
@@ -2,7 +2,7 @@
 #define TEMPERATURE_MAX			45
 #define SOC_MIN				20
 #define SOC_MAX				80
-#define CHARGERATE_LIMIT	0.8
+#define 	0.8
 
 #define BATTERY_PARAM_NOTINRANGE(param, min, max)		((param < min) || (param > max))
 #define CHARGERATE_NOTINRANGE(param, limit)				  (param > limit)
