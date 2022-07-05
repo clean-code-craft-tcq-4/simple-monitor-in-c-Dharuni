f93bd5546db7f4af39a8b2493db4e017dcfd204f
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-07-05 21:21:22 +0530

    Create BatteryCheck.h

---
+++ BatteryCheck.h
@@ -0,0 +1,8 @@
+#define TEMPERATURE_MIN			0
+#define TEMPERATURE_MAX			45
+#define SOC_MIN				20
+#define SOC_MAX				80
+#define CHARGERATE_LIMIT	0.8
+
+#define BATTERY_PARAM_NOTINRANGE(param, min, max)		((param < min) || (param > max))
+#define CHARGERATE_NOTINRANGE(param, limit)				  (param > limit)
